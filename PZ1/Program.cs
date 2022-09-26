d9af815073aacde3f00de1c38461fc71bd0fb64f
Alexandr Logvinov <ato31ato@gmail.com>
2022-09-26 21:41:37 +0300

    Add project files.

---
+++ PZ1/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PZ1
+{
+
+    public abstract class Cipher<T>
+    {
+        protected readonly string alfabet;
+        protected readonly string fullAlfabet;
+        protected char[] fullAlfabetArray;
+        protected int alphabetLength = 0;
+        public Cipher(string locale)
+        {
+            if (locale == "EN")
+            {
+                alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            }
+            else
+            {
+                alfabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+            }
+            fullAlfabet = alfabet.ToLower() + alfabet;
+            alphabetLength = fullAlfabet.Length;
+            fullAlfabetArray = fullAlfabet.ToCharArray();
+        }
+        public abstract string Encrypt(string plainMessage, T key);
+        public abstract string Decrypt(string encryptedMessage, T key);
+    }
+
+    public class CaesarCipher : Cipher<int>
+    {
+        public CaesarCipher(string locale) : base(locale)  { }
+        private string CodeEncode(string text, int k)
+        {
+            var fullAlfabet = alfabet.ToLower() + alfabet;
+            var q = fullAlfabet.Length;
+            var fullAlfabetArray = fullAlfabet.ToCharArray();
+            var retVal = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                var c = text[i];
+                var index = Array.IndexOf(fullAlfabetArray, c);
+                if (index < 0)
+                {
+                    retVal += c.ToString();
+                }
+                else
+                {
+                    var codeIndex = (q + index + k) % q;
+                    if (Char.IsUpper(c))
+                        retVal += Char.ToUpper(fullAlfabet[codeIndex]);
+                    else
+                        retVal += Char.ToLower(fullAlfabet[codeIndex]);
+                }
+            }
+
+            return retVal;
+        }
+        public override string Encrypt(string plainMessage, int key)
+            => CodeEncode(plainMessage, key);
+        public override string Decrypt(string encryptedMessage, int key)
+            => CodeEncode(encryptedMessage, -key);
+    }
+
+    public class Token
+    {
+        public char TokenStr { get; set; }
+        public int Position { get; set; }
+    }
+
+    public class VigenereCipher : Cipher<string>
+    {
+
+        public VigenereCipher(string local) : base(local) { }
+
+        //генерация повторяющегося пароля
+        private string GetRepeatKey(string s, int n)
+        {
+            var p = s;
+            while (p.Length < n)
+            {
+                p += p;
+            }
+
+            return p.Substring(0, n);
+        }
+
+        private string Vigenere(string text, string password, bool encrypting = true)
+        {
+            var gamma = GetRepeatKey(password, text.Length);
+            var retValue = "";
+            var q = fullAlfabetArray.Length;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var letterIndex = fullAlfabet.IndexOf(text[i]);
+                var codeIndex = fullAlfabet.IndexOf(gamma[i]);
+                if (letterIndex < 0)
+                {
+                    retValue += text[i].ToString();
+                }
+                else
+                {
+
+                    var index = (q + letterIndex + ((encrypting ? 1 : -1) * codeIndex)) % q;
+                    var letter = fullAlfabet[index].ToString();
+                    retValue += Char.IsUpper(text[i]) ? letter.ToUpper() : letter.ToLower();
+                }
+            }
+
+            return retValue;
+        }
+
+
+        private (string result, List<Token> tokens) clearFromTokens(string input)
+        {
+            List<Token> allTokens = new List<Token>();
+            string clearedStr = "";
+            char[] tokens = new char[] { ' ', ',', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Array.Exists<char>(tokens, x => x == input[i]))
+                {
+                    allTokens.Add(new Token() { TokenStr = input[i], Position = i });
+                }
+                else
+                {
+                    clearedStr += input[i];
+                }
+            }
+
+            return (clearedStr, allTokens);
+        }
+
+        private string putTokensBack(string str, List<Token> tokens)
+        {
+            var newStr = str;
+            foreach (var t in tokens)
+            {
+                newStr = newStr.Insert(t.Position, t.TokenStr.ToString());
+            }
+            return newStr;
+        }
+
+        public override string Decrypt(string plainMessage, string password)
+        {
+            var clearedResult = clearFromTokens(plainMessage);
+
+            var decrypted = Vigenere(clearedResult.result, password, false);
+
+            decrypted = putTokensBack(decrypted, clearedResult.tokens);
+            return decrypted;
+        }
+
+        public override string Encrypt(string plainMessage, string password)
+        {
+            var clearedResult = clearFromTokens(plainMessage);
+            var encrypted = Vigenere(clearedResult.result, password);
+            encrypted = putTokensBack(encrypted, clearedResult.tokens);
+            return encrypted;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("~~Task 1");
+            var caesar = new CaesarCipher("RU");
+            var message = "Р гясисэс бахеюсэс, ида ая – гцьвцдямы ёъщъь. Дсьац шц щсфсхаияац умвсшцяъц эъзс,";
+            var secretKey = 18;
+            Console.WriteLine("Расшифрованное сообщение: {0}", caesar.Decrypt(message, secretKey));
+            Console.WriteLine();
+            Console.WriteLine("~Task 2");
+            var visioner = new VigenereCipher("RU");
+            var message2 = "прщуым ъй епръвцниф ёдой эюцмы уихумлфгя апс йамфщ у 1984 гопь. ан пьнхпоччшил, гыа есчс ты пъзуиллън воухажнъъдь иэшальучуатз к ьачръдве ъыьрыючфо кчжиа ишз ъли ычитонды адьнг алфъм, то иыа лидсэо бж ъэожщьп пръяцдуьь сутрцдиффусциф кгякъла смжъэа. дъффое нщцмя фмця шлхъра ъъдавлфссь нъцго чсйь кьигивът ьриыыагрлэъчеэуай гъфавоччюкох, ца в 2000 гъме, бллладаьз аднът ъзвръднох ьрзвфхастф к оллфшдичръьой цщъптълваффс, ъдей ьхалъън воыфатиюе у жиуцн.";
+            var code2 = "алиса";
+
+            Console.WriteLine("Расшифрованное сообщение: {0}", visioner.Decrypt(message2, code2));
+
+            Console.WriteLine("~Task 3");
+            var message3 = "зеэкле он ьаиоёнюби йыяв свнэу зммдеашъп шдх бреиэ к 1984 ужда. чю зрсмажлыпщд, чяч хйлх йл золкщдаюе тжзщччёоюым бсьчьфзыкркь о урпеюытэ ояубутыля глкар бмл сьб пыагжвзт рьрсъ рдиюд, гж эяч ьбшхфя щы юфяянаж аиогнфлра идкеъыщмичижби оъпгопч веыюфр. ьошляэ вэнэч ирнп ращсбш оюыръашивф вюнуж лхбм грмъщъоц уббпячуиабсзэсччъ ыошчтжлыхыжй, ъч т 2000 ыорь, сдапчфшрл чфёоц сшъеюыюжй азшъищчвки о ёьдиьыщпеюуяв кэсакопщрмих, сфэю амрдоюе тжпшчгбти к чбзъе.";
+            var code3 = "Шамир";
+
+            Console.WriteLine("Расшифрованное сообщение: {0}", visioner.Decrypt(message3, code3));
+        }
+    }
+}
---
+++ PZ2/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PZ2
+{
+    public class Token
+    {
+        public char TokenStr { get; set; }
+        public int Position { get; set; }
+    }
+
+    class Program
+    {
+        public readonly static IReadOnlyDictionary<char, string> val2Key;
+        public readonly static IReadOnlyDictionary<string, char> key2Val;
+        static Program()
+        {
+            val2Key = new Dictionary<char, string> {
+                {'А',"1"},  {'Б',"81"}, {'В',"82"},  {'Г',"83"},  {'Д',"84"},   {'Е',"4"},
+                {'Ж',"85"},   {'З',"86"}, {'И',"2"},  {'К',"87"},   {'Л',"88"},   {'М',"89"},
+                {'Н',"6"},  {'О',"7"}, {'П',"80"},   {'Р',"91"}, {'С',"5"},  {'Т',"3"}, {'У',"92"},
+                {'Ф',"93"}, {'Х',"94"},  {'Ц',"95"}, {'Ч',"96"},   {'Ш',"97"}, {'Щ',"98"},   {'Ъ',"99"},
+                {'Ы',"90"},  {'Ь',"01"},{'Э',"02"},  {'Ю',"03"}, {'Я',"04"}, {' ',"00"},
+                {'1',"711"},  {'2',"712"},{'3',"713"},  {'4',"714"}, {'5',"715"},{'6',"716"},
+                {'7',"717"},{'8',"718"},  {'9',"719"}, {'0',"710"} };
+
+            key2Val = val2Key.ToDictionary(kv => kv.Value, kv => kv.Key);
+        }
+
+        public static string EncodeByTable(string s)
+        {
+            return string.Concat(s.ToUpper().ToCharArray()
+                .Where(c => val2Key.ContainsKey(c)).Select(c => val2Key[c]));
+        }
+
+        public static string DecodedTable(string s)
+        {
+            return string.Concat(Regex.Matches(s, "71.|8.|9.|0.|.").Cast<Match>()
+                .Where(m => key2Val.ContainsKey(m.Value)).Select(m => key2Val[m.Value]));
+        }
+
+        public static (List<Token> tokens, string pureString) ClearText(string input)
+        {
+            char[] tokens = new char[] { ',', '.' };
+
+            List<Token> allTokens = new List<Token>();
+            string clearedStr = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Array.Exists<char>(tokens, x => x == input[i]))
+                {
+                    allTokens.Add(new Token() { TokenStr = input[i], Position = i });
+                }
+                else
+                {
+                    clearedStr += input[i];
+                }
+            }
+            return (allTokens, clearedStr);
+        }
+
+        public static string PutTokensBack(List<Token> tokens, string input)
+        {
+            var result = input;
+            foreach (var token in tokens)
+            {
+                result = result.Insert(token.Position, token.TokenStr.ToString());
+            }
+            return result;
+        }
+
+        public static string GetRepeatKey(string s, int n)
+        {
+            var p = s;
+            while (p.Length < n)
+            {
+                p += p;
+            }
+
+            return p.Substring(0, n);
+        }
+
+
+        private static string DecodeInput(string encodedInput, string key) 
+        {
+            
+            var encodedText = encodedInput;
+            var repeatedKey = GetRepeatKey(key, encodedText.Length);
+            
+            var decodedText = "";
+            var encodedRepeatedKey = EncodeByTable(repeatedKey);
+            for (int i = 0; i < encodedText.Length; i++)
+            {
+                var temp = int.Parse(encodedText[i].ToString()) - int.Parse(encodedRepeatedKey[i].ToString());
+                if (temp < 0)
+                {
+                    temp = 10 + temp;
+                }
+                decodedText += temp.ToString();
+            }
+            var text = DecodedTable(decodedText);
+
+            return text;
+        }
+
+        private static string EncodeInput(string originalText, string key) 
+        {
+            var encodedTextByTable = EncodeByTable(originalText);
+            var encodedKeyByTable = EncodeByTable(key);
+            var repeatedEncodedKey = GetRepeatKey(encodedKeyByTable, encodedTextByTable.Length);
+
+            string encodedTextByKey = "";
+            for (int i = 0; i < encodedTextByTable.Length; i++)
+            {
+                var temp = (int.Parse(encodedTextByTable[i].ToString()) + int.Parse(repeatedEncodedKey[i].ToString())) % 10;
+                encodedTextByKey += temp.ToString();
+            }
+            return encodedTextByKey;
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("~~~Task 1");
+            var decodedTextPart1 = DecodeInput("6719882196864085864979275245", "лес");
+            System.Console.WriteLine(decodedTextPart1);
+            System.Console.WriteLine("~~~Task 2");
+            
+            string inputInfo = "Александр Логвнов 3114.2112.1.1234567890 1 2 3 4 5 6 7 8 9 0 10 77 171 717";
+            string key = "олень";
+
+            var clearedStringInfo = ClearText(inputInfo);
+            var textToEncode = clearedStringInfo.pureString.ToUpper();
+
+            var encodedText = EncodeInput(textToEncode, key);
+            Console.WriteLine("EncodedText: " + encodedText);
+            var decodedText = DecodeInput(encodedText, key);
+            Console.WriteLine("DecodedText: " + decodedText);
+        }
+    }
+}
