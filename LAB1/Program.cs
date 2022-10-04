c1a6ae2af5656edd3d90d14b00c817ebc9939d70
Alexandr Logvinov <ato31ato@gmail.com>
2022-10-04 20:44:35 +0300

    lab1 raw code

---
+++ LAB1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LAB1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
